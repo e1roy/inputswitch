e1ef60225639f7d144ceda3d6e45aec76a49e374
elroysu <elroysu@tencent.com>
2026-02-08 14:26:19 +0800

    Initial Commit

---
+++ inputswitchTests/inputswitchTests.swift
@@ -0,0 +1,17 @@
+//
+//  inputswitchTests.swift
+//  inputswitchTests
+//
+//  Created by 苏卫东 on 2026/2/8.
+//
+
+import Testing
+@testable import inputswitch
+
+struct inputswitchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ inputswitchUITests/inputswitchUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  inputswitchUITestsLaunchTests.swift
+//  inputswitchUITests
+//
+//  Created by 苏卫东 on 2026/2/8.
+//
+
+import XCTest
+
+final class inputswitchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
